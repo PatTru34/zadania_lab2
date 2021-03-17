4a4c8e36753ad5dce762a201e5ccf5974fbbe150
PatTru34 <PatTru34>
2021-03-17 08:57:51 +0100

    Zadania 2

--- main.py
+++ main.py
@@ -1,1 +1,91 @@
-# 
+# Zadanie 1
+# lista = ["Piłka nożna","Siatkówka","Hokej","Piłka ręczna"]
+# print(lista)
+# lista.reverse()
+# lista.append("koszykówka")
+# lista.append("Bieganie")
+# print(lista)
+
+# Zadanie 2
+# slownik={'np':'na przykład',"jw":"jak wyżej","wyd":'Wydanie','tj':"to jest",'pt':"pod tytułem"}
+# print(slownik)
+
+# Zadanie 3
+# gry={"1":"Liga Legend","2":"Overwatch","3":"The Binding of Isaac","4":"SF Game","5":"Genshin Impact","honorable mention":"Rainbow Six Siege"}
+# print(gry)
+# l = len(gry.keys())
+# print("W słowniku jest %d elementów" % (l))
+
+# Zadanie 4
+# zdanie=input("Podaj zdanie:")
+# print(zdanie)
+# print(zdanie.count('a'))
+
+# Zadanie 5
+# import sys as system
+# system.stdout.write("podaj liczby: ")
+# a = system.stdin.readline()
+# b = system.stdin.readline()
+# c = system.stdin.readline()
+#
+# liczba = pow(int(a), int(b)) + int(c)
+#
+# system.stdout.write(str(liczba))
+
+# Zadanie 6
+
+# a=input("Wprowadz a")
+# a=int(a)
+# b=input("Wprowadz b")
+# b=int(b)
+# c=input("Wprowadz c")
+# c=int(c)
+# if a>b>=c:
+#     print("liczba a jest największa")
+# elif a>b>=c:
+#     print("liczba a jest największa")
+# elif b>a>=c:
+#     print("liczba b jest największa")
+# elif b>c>=a:
+#     print("liczba b jest największa")
+# elif c>a>=b:
+#     print("liczba c jest największa")
+# elif c>b>=a:
+#     print("Liczba c jest największa")
+# else:
+#     print("Liczby są równe")
+# Zadanie 7
+
+# lista=[2,3.8,12,4.2,4]
+# for i in lista:
+#     print(i**2)
+# Zadanie 8
+# i=1
+# lista=[]
+# while(i<=10):
+#     liczba=input("Wprowadz liczbe: ")
+#     liczba=int(liczba)
+#     i+=1
+#     if liczba%2==0:
+#         lista.append(liczba)
+#
+# print(lista)
+# Zadanie 9
+# lista = [1,2,3,4,5]
+#
+# for i in lista:
+#     if i % 2 == 0:
+#         print("E")
+#     else:
+#       print("EEEEEE")
+# Zadanie 10
+# from math import *
+# liczba = input("Podaj liczbę: ")
+#
+# liczba = int(liczba)
+#
+# if liczba<0:
+#     print("Liczba nie może być ujemna")
+# else:
+#     d = sqrt(liczba)
+#     print(d)
